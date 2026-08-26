9d48445562d9b07c2df283e17562ca24fc47b2d8
Lucas Renan Moraes Ribeiro <renanmoraes.ribeiro@gmail.com>
2026-08-25 21:29:02 -0300

    feat: cria estrutura inicial e classe base Veiculo

---
+++ src/AutoCheck.ConsoleApp/Models/Veiculo.cs
@@ -0,0 +1,36 @@
+namespace AutoCheck.ConsoleApp.Models;
+
+public abstract class Veiculo
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public double Quilometragem { get; set; }
+    public List<ItemVistoria> VistoriaRealizada { get; set; }
+
+    public Veiculo(string marca, string modelo, int ano, double quilometragem)
+    {
+        this.Marca = marca;
+        this.Modelo = modelo;
+        this.Ano = ano;
+        this.Quilometragem = quilometragem;
+        this.VistoriaRealizada = new List<ItemVistoria>();
+    }
+
+    public void AdicionarItemVistoriado(string nome, string status)
+    {
+        ItemVistoria item = new ItemVistoria(nome, status);
+        this.VistoriaRealizada.Add(item);
+    }
+
+    public virtual List<string> ObterChecklistObrigatorio()
+    {
+        List<string> checklist = new List<string>();
+
+        checklist.Add("Nível de Óleo do Motor");
+        checklist.Add("Bateria e Sistema Elétrico");
+        checklist.Add("Documentação Regularizada");
+
+        return checklist;
+    }
+}
